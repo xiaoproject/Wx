e33c81fef8486954c6dea9836b8dfe7cd0c712bd
xiaoproject <626223818@qq.com>
2017-09-06 14:12:08 +0800

    修复定位Bug

--- api.php
+++ api.php
@@ -3,6 +3,7 @@ define("TOKEN", "weixin");
 include dirname(__FILE__) . "/media.php";
 include dirname(__FILE__) . "/Lib/WeChatApi.class.php";
 include dirname(__FILE__) . "/Lib/WeChat.class.php";
+include dirname(__FILE__) . "/Lib/ThirdApi.class.php";
 
 class WxApi extends Wechat
 {
@@ -25,13 +26,9 @@ class WxApi extends Wechat
         if ($this->sendType = 'location') {
             $lat = $this->lat; // 纬度
             $lng = $this->lng; // 经度
-//            $this->reText('$lng:'.$lng);
-//            die();
 
             // 获取地址
             $lbs_url = ThirdApi::getApiFromLBS($lat, $lng);
-            $this->reText('$lbs_url:'.$lbs_url);
-            die();
 
             // 使用curl的get方式获取json数据
             $jsonStr = $this->CurlRequest($lbs_url);
@@ -39,10 +36,11 @@ class WxApi extends Wechat
             // assoc: true 返回数组 默认返回对象
             $arr = json_decode($jsonStr, true);
 
-            $formatted_address = $arr['status'];
+            $formatted_address = $arr['result']['formatted_address'];
 
-            $this->reText('status:'.$formatted_address);
+            $this->reText('status:' . $formatted_address);
             die();
+
         }
     }
 
