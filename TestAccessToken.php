96bb838a278399c9469d29c3b59e3b47c13dbe22
xiaoproject <626223818@qq.com>
2017-09-07 19:52:45 +0800

    群发

--- TestAccessToken.php
+++ TestAccessToken.php
@@ -8,5 +8,24 @@
  */
 include_once dirname(__FILE__) . '/Lib/WeChat.class.php';
 
-$wechat = new WeChat();
-echo $wechat->GetAccessToken();
+class TestAccessToken extends WeChat
+{
+    public function sendAll()
+    {
+        $url = WeChatApi::getApiUrl('api_send_mass');
+        $token = $this->GetAccessToken();
+        $data = array('access_token' => $token);
+
+        $json = $this->CurlPostJson($url, $data);
+        return $json;
+
+    }
+
+}
+
+
+$wechat = new TestAccessToken();
+var_dump($wechat->sendAll());
+
+
+//echo $wechat->GetAccessToken();
