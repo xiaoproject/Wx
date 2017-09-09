b3ba375d288cbd152c40c379d5cb35035cec62fc
xiaoproject <626223818@qq.com>
2017-09-09 21:29:07 +0800

    获取openID

--- userinfo/mine.php
+++ userinfo/mine.php
@@ -2,9 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Title</title>
+
 </head>
 <body>
+    <?php
+    //第2步：获取授权的code;
+    $code = $_GET['code'];
+    echo $code;
+
+    ?>
     hello world
 </body>
 </html>
